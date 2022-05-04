3b0b61dc3d9ada78dd26000c656394dd89d8a21c
joelfrost <joelfrost4@gmail.com>
2022-05-04 17:05:48 +0100

    Added new routes for new pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import Login from './components/User/Login';
 import SignUp from './components/User/SignUp';
 import Library from './components/Library';
+import Logout from './components/User/Logout';
+import Profile from './components/User/Profile';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path='/' element={<Login />} />
+        <Route exact path='/' element={<Library />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/library" element={<Library />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,26 @@ import logo from "./../images/logo.png"
 
 function Navigation() {
 
+    if (localStorage.getItem('logged_in') !== "true") {
+        return (
+            <>
+                <Navbar bg="dark" variant="dark" fixed="top">
+                    <Container>
+                        <Navbar.Brand href="/library">
+                            <img src={logo} width="50" height="30" className="d-inline-block align-top" alt="Logo" />
+                            Game Manager</Navbar.Brand>
+                        <Nav className="ml-auto">
+                            <Nav.Link href="/login">Login</Nav.Link>
+                            <Nav.Link href="/signup">Sign Up</Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
+            </>
+        );
+    }
+
     return (
+
         <>
             <Navbar bg="dark" variant="dark" fixed="top">
                 <Container>
@@ -12,7 +31,11 @@ function Navigation() {
                         Game Manager</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="/library">Library</Nav.Link>
-                        <Nav.Link href="/">Login</Nav.Link>
+                        <Nav.Link href="/profile">Profile</Nav.Link>
+
+                    </Nav>
+                    <Nav className="ml-auto">
+                        <Nav.Link href="/logout">Logout</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
