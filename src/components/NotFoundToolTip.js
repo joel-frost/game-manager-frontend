735ac16c1c3c7966009bdc70a01584f679be49f4
joelfrost <joelfrost4@gmail.com>
2022-05-15 18:10:52 +0100

    Removed unusued import

--- src/components/NotFoundToolTip.js
+++ src/components/NotFoundToolTip.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { OverlayTrigger, Tooltip, Button } from 'react-bootstrap';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { FaQuestionCircle } from 'react-icons/fa';
 
 export default function NotFoundToolTip() {
